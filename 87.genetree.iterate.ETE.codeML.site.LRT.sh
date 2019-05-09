f86f1b513c90baa6b7eda16156e6495807524fb2
Robert W Murdoch <robert.waller.murdoch@gmail.com>
2019-05-09 15:09:36 -0400

    Create 87.genetree.iterate.ETE.codeML.site.LRT.sh

---
+++ 87.genetree.iterate.ETE.codeML.site.LRT.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# this script loops through trees and trimmed alignments in their own directories
+# and then subjects them to LRT testing using the ETE3 codeML/src implementation
+# this script does NOT parse the LRT results, which are in the stout file
+
+# This script should be run from the same folder where the folder containing the gene trees and alignments are
+
+export PATH=~/anaconda_ete/bin:$PATH
+
+
+
+for f in /genestree/*
+	do
+	gene=${f##*/}
+	mkdir "results_genetree_87/$gene"
+	ete3 evol \
+	-t /genestree/$gene \
+	--alg /genesalign/"${gene}.trim" \
+	--models M8 M7 \
+	--tests M7,M8 \
+	--codeml_param verbose,0 \
+	-i "results_genetree_87/$gene/image.png" \
+	--histface bar \
+	-C 8 \
+	-o "results_genetree_87/$gene"/ > "results_genetree_87/$gene/$gene.stdout.txt"
+	echo -n "${gene}" >> ETE3_results_genetree_87.csv
+	echo -n "," >> ETE3_results_genetree_87.csv
+	lnL1=$(grep lnL results_genetree_87/$gene/M7*/rst | grep -oE '\-[0-9]+.[0-9]+')
+	echo -n "$lnL1" >> ETE3_results_genetree_87.csv
+	echo -n "," >> ETE3_results_genetree_87.csv
+	lnL2=$(grep lnL results_genetree_87/$gene/M8*/rst | grep -oE '\-[0-9]+.[0-9]+')
+	echo -n "$lnL2" >> ETE3_results_genetree_87.csv
+	echo -n "," >> ETE3_results_genetree_87.csv
+	LRTp=$(grep '^\s*M7' results_genetree_87/$gene/*.txt | grep -oE '[^ ]+$')
+	echo -n "$LRTp" >> ETE3_results_genetree_87.csv 
+	echo -n "," >> ETE3_results_genetree_87.csv
+	w1=$(grep Average results_genetree_87/$gene/*.txt | grep -oE '[0-9]+.[0-9]+' | sed -n '1p')
+	echo -n "$w1" >> ETE3_results_genetree_87.csv
+	echo -n "," >> ETE3_results_genetree_87.csv
+	w2=$(grep Average results_genetree_87/$gene/*.txt | grep -oE '[0-9]+.[0-9]+' | sed -n '2p')
+	echo "$w2" >> ETE3_results_genetree_87.csv
+done
+
